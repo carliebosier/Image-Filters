64eb465a93d6699cd4a11687a1131472ac78f470
Carlie Bosier <159051907+carliebosier@users.noreply.github.com>
2024-11-21 19:04:21 -0500

    creates a grayscale image of the input image

---
+++ grayscale.py
@@ -0,0 +1,14 @@
+import common
+grayscale_image = common.read_file("logo.jpeg")
+
+print(grayscale_image[0][0])
+length = len(grayscale_image)
+width = len(grayscale_image[0])
+
+for row in range(length):
+    for col in range(width):
+        r, g, b = (grayscale_image[row][col])
+        grays = int((r + g + b)/3)
+        grayscale_image[row][col] = (grays, grays, grays)
+
+common.write_file("new_logo.jpeg", grayscale_image)
